aeccf1b774eb2acebfc600d14dd3f097a8965360
droll-accelerator-1483 <srsani@gmail.com>
2019-07-20 17:35:45 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,6 +2,6 @@
 first_number = 4
 second_number = 10
 sum = first_number+second_number
-sum = first_number-second_number
-sum = first_number*second_number
+difference = first_number-second_number
+product = first_number*second_number
 sum = first_number/second_number
